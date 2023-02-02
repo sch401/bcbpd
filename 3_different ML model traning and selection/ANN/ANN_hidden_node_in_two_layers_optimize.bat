24d106cb7de45a04bb757622a987952a496402ad
sch401 <supersong97@icloud.com>
2023-02-02 14:30:22 +0800

    Create ANN_hidden_node_in_two_layers_optimize.bat

---
+++ 3_different ML model traning and selection/ANN/ANN_hidden_node_in_two_layers_optimize.bat
@@ -0,0 +1,34 @@
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 7 7
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 7 6
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 7 5
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 7 4
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 7 3
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 7 2
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 6 7
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 6 6
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 6 5
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 6 4
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 6 3
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 6 2
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 5 7
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 5 6
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 5 5
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 5 4
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 5 3
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 5 2
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 4 7
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 4 6
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 4 5
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 4 4
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 4 3
+C:/Users/sch401/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 4 2
+C:/Users/sch301/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 3 7
+C:/Users/sch301/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 3 6
+C:/Users/sch301/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 3 5
+C:/Users/sch301/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 3 4
+C:/Users/sch301/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 3 3
+C:/Users/sch301/AppData/Local/Programs/Python/Python310/python.exe "D:/Doctor_Thesis/database and website/code/3_Different ML model training and selection with selected feature/ANNoptimize/ANN2findfirstsecond.py" 3 2
+
+
+
+pause
